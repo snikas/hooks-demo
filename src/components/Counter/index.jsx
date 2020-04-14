4c0aac0c107515aa54da6e04e17ee6d74f5858f2
steven Akins <steven.akins@revelsystems.com>
2020-04-14 09:32:32 -0400

    useReducer hook

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { ButtonContainer } from './styles';
 
+function init(initialCount) {
+    return initialCount;
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return state + 1;
+      case 'decrement':
+        return state - 1;
+      case 'reset':
+        return init(action.payload);
+      default:
+        throw new Error();
+    }
+  }
+
 const Counter = ({ initialCount }) => {
-    const [count, setCount] = useState(initialCount);
+    const [count, dispatch] = useReducer(reducer, initialCount, init);
 
     useEffect(() => {
-        setCount(initialCount);
+        dispatch({ type: 'reset', payload: initialCount })
     }, [initialCount])
 
     return (
@@ -13,11 +30,11 @@ const Counter = ({ initialCount }) => {
             Count: {count}
             <ButtonContainer>
                 <button
-                onClick={() => setCount(initialCount)}>
+                onClick={() => dispatch({ type: 'reset', payload: initialCount })}>
                 Reset
                 </button>
-                <button onClick={() => setCount(prevState => prevState - 1)}>-</button>
-                <button onClick={() => setCount(prevState => prevState + 1)}>+</button>
+                <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+                <button onClick={() => dispatch({ type: 'increment' })}>+</button>
             </ButtonContainer>
         </>
         );
